846e21e779a9e4e403eb880454eaed147a95acd3
Ryan Soury <ryan@webdoodle.com.au>
2022-06-21 15:01:23 +1000

    update to campaign aql to use wallet id as key

--- packages/admin/src/cmd.ts
+++ packages/admin/src/cmd.ts
@@ -2,11 +2,13 @@ import { Command } from "commander";
 import schemaCmd from "./cmd/schema";
 import definitionCmd from "./cmd/definition";
 import tileCmd from "./cmd/tile";
+import arweaveCmd from "./cmd/arweave";
 
 const program = new Command();
 
 program.addCommand(schemaCmd);
 program.addCommand(tileCmd);
 program.addCommand(definitionCmd);
+program.addCommand(arweaveCmd);
 
 program.parse(process.argv);
