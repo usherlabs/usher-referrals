5c57f3c412651a3b3668ed67ca4c0f732b41104d
Ryan Soury <ryan@webdoodle.com.au>
2022-06-26 20:25:33 +1000

    developed a test for connection to arangodb -- determined that a tcp:// protocol is required for connecting to remote arango

---
+++ packages/app/tests/utils/arango-client.test.ts
@@ -0,0 +1,19 @@
+import { aql } from "arangojs";
+import { getArangoClient } from "@/utils/arango-client";
+
+const arango = getArangoClient();
+
+describe("Arango Client Test", () => {
+	it("Connects to the Database", async () => {
+		const cursor = await arango.query(aql`
+			FOR c IN Campaigns
+				RETURN c
+		`);
+		let count = 0;
+		for await (const res of cursor) {
+			console.log(res);
+			count += 1;
+		}
+		expect(count).toBeGreaterThan(0);
+	});
+});
