2ac63989719c8dd91050b4c199e8d1ec418d5a9b
Ryan Soury <ryan@webdoodle.com.au>
2022-07-10 18:02:47 +1000

    developed the reward claim endpoint -- requires testing

--- packages/app/src/env-config.ts
+++ packages/app/src/env-config.ts
@@ -2,7 +2,9 @@
 
 export const isProd = process.env.NODE_ENV === "production";
 export const isTest = process.env.NODE_ENV === "test";
-export const appName = `${process.env.APP_NAME}@${process.env.APP_VERSION}`;
+export const appPackageName = process.env.APP_NAME;
+export const appVersion = process.env.APP_VERSION;
+export const appName = `${appPackageName}@${appVersion}`;
 export const useSeedData = process.env.NEXT_PUBLIC_USE_SEED_DATA === "true";
 
 export const ngrokUrl = process.env.NEXT_PUBLIC_NGROK_URL;
--- packages/app/src/pages/api/claim.ts
+++ packages/app/src/pages/api/claim.ts
@@ -21,6 +21,7 @@ import withAuth from "@/server/middleware/auth";
 import { getArweaveClient } from "@/utils/arweave-client";
 import { FEE_MULTIPLIER } from "@/constants";
 import handleException from "@/utils/handle-exception";
+import { appPackageName, appVersion } from "@/env-config";
 
 const handler = getHandler();
 
@@ -117,22 +118,26 @@ handler.use(withAuth).post(async (req: AuthApiRequest, res: ApiResponse) => {
 		});
 	}
 
-	// Get rewards from partnership and rewards claimed from campaign
-	const pCursor = await arango.query(aql`
-		LET rewards_claimed = (
-			FOR cl IN 1..1 OUTBOUND partnership Engagements
-				FILTER STARTS_WITH(cl._id, "Claims")
-				COLLECT AGGREGATE amount = SUM(cl.amount)
-				RETURN amount
-		)
-		RETURN TO_NUMBER(rewards_claimed)
-	`);
-	const pResults = await pCursor.all();
-	const [rewardsClaimed] = pResults; // will always return an array with a single integer result.
+	req.log.debug("Campaign and Partnership indexed data fetched");
 
 	// Determine withdraw amount -- by calculating remaining rewards
 	let rewardsToPay = partnershipData.rewards || 0;
 	if (typeof campaignData.reward.limit === "number") {
+		// Get rewards from partnership and rewards claimed from campaign
+		const pCursor = await arango.query(aql`
+			LET rewards_claimed = (
+				FOR cl IN 1..1 OUTBOUND CONCAT("Partnerships/", ${partnershipId}) Engagements
+					FILTER STARTS_WITH(cl._id, "Claims")
+					COLLECT AGGREGATE amount = SUM(cl.amount)
+					RETURN amount
+			)
+			RETURN TO_NUMBER(rewards_claimed)
+		`);
+		const pResults = await pCursor.all();
+		const [rewardsClaimed] = pResults; // will always return an array with a single integer result.
+
+		req.log.debug({ rewardsClaimed }, "Previously Rewards Claims fetched");
+
 		// Check if the Campaign is limited at all
 		const remainingRewards = campaignData.reward.limit - rewardsClaimed;
 		if (rewardsToPay > remainingRewards) {
@@ -141,29 +146,152 @@ handler.use(withAuth).post(async (req: AuthApiRequest, res: ApiResponse) => {
 	}
 
 	// Ensure that amount to be paid is greater than amount in internal wallet -- otherwise send whatevers in the wallet and update partnership amount
-	const did = await getAppDID();
 	if (campaignData.chain === Chains.ARWEAVE) {
 		const balance = await arweave.wallets.getBalance(
 			campaignData._internal.address
 		);
 		const arBalanceStr = arweave.ar.winstonToAr(balance);
+		const winstonBalance = parseFloat(balance);
 		let arBalance = parseFloat(arBalanceStr);
-		if (Number.isNaN(arBalance)) {
+		if (Number.isNaN(arBalance) || Number.isNaN(winstonBalance)) {
 			arBalance = 0;
 		}
-		if (rewardsToPay > arBalance) {
-			rewardsToPay = arBalance;
+
+		if (arBalance === 0) {
+			req.log.error("Insufficient funding for Campaign");
+			return res.status(402).json({
+				success: false,
+				data: {
+					to,
+					amount: 0
+				}
+			});
+		}
+
+		let fee = rewardsToPay * FEE_MULTIPLIER; // x * 0.1
+		const totalToPay = fee + rewardsToPay;
+		if (totalToPay > arBalance) {
+			fee = arBalance * FEE_MULTIPLIER;
+			rewardsToPay = arBalance - fee;
 		}
 
 		// Transfer fee amount minus gas x2 from previous transaction to platform wallet
 		try {
+			const did = await getAppDID();
 			const jwe = JSON.parse(Base64.decode(campaignData._internal.key));
 			const dec = await did.decryptJWE(jwe);
 			const raw = uint8arrays.toString(dec);
 			const jwk = JSON.parse(raw);
 
-			const fee = rewardsToPay * (1 - FEE_MULTIPLIER);
-			// TODO: Continue payout development
+			const rewardTx = await arweave.createTransaction(
+				{
+					target: to,
+					quantity: arweave.ar.arToWinston(`${rewardsToPay}`)
+				},
+				jwk
+			);
+			const txTags = [
+				["Application", "Usher"],
+				["UsherCampaign", campaignRef.address],
+				["UsherPartnership", partnershipId]
+			];
+			if (appPackageName && appVersion) {
+				txTags.push([appPackageName, appVersion]);
+			}
+
+			fee -= parseFloat(arweave.ar.winstonToAr(rewardTx.reward)) * 2; // ensure there's enough gas for the transfers.
+			const feeTx = await arweave.createTransaction({
+				target: to,
+				quantity: arweave.ar.arToWinston(`${fee}`)
+			});
+
+			req.log.debug({ fee, rewardsToPay }, "Fee and reward calculated");
+
+			// Tags
+			txTags.forEach(([tagName, tagVal]) => {
+				rewardTx.addTag(tagName, tagVal);
+				feeTx.addTag(tagName, tagVal);
+			});
+			rewardTx.addTag("UsherTransferType", "Reward");
+			feeTx.addTag("UsherTransferType", "Fee");
+
+			await arweave.transactions.sign(rewardTx, jwk);
+			await arweave.transactions.sign(feeTx, jwk);
+
+			const [rewardTxResponse, feeTxResponse] = await Promise.all([
+				arweave.transactions.post(rewardTx),
+				arweave.transactions.post(feeTx)
+			]);
+
+			if (rewardTxResponse.status !== 200) {
+				req.log.error(
+					{ rewardTxResponse, rewardTx, rewardsToPay },
+					"Failed to submit reward transaction"
+				);
+				return res.json({
+					success: false,
+					data: {
+						to,
+						amount: rewardsToPay
+					}
+				});
+			}
+			if (feeTxResponse.status !== 200) {
+				req.log.warn(
+					{ feeTxResponse, feeTx, fee },
+					"Failed to submit fee transaction"
+				);
+				return res.json({
+					success: false,
+					data: {
+						to,
+						amount: rewardsToPay
+					}
+				});
+			}
+
+			req.log.info(
+				{ fee, rewardsToPay, rewardTx: rewardTx.id, feeTx: feeTx.id },
+				"Fee and reward transfers complete"
+			);
+
+			// Index Claim, Reduce Remaining Rewards for Partnership
+			const indexCursor = await arango.query(aql`
+				INSERT {
+					amount: ${rewardsToPay},
+					to: ${to},
+					fee: ${fee}
+					ar_txs: {
+						reward: ${rewardTx.id},
+						fee: ${feeTx.id}
+					},
+					created_at: ${Date.now()}
+				} INTO Claims OPTIONS { waitForSync: true }
+				LET c = NEW
+				INSERT {
+					_from: p._id,
+					_to: c._id
+				} INTO Engagements
+				LET e = NEW
+				LET p = DOCUMENT("Partnerships", ${partnershipId})
+				UPDATE { _key: p._key } WITH { rewards: p.rewards - ${rewardsToPay} } IN Partnerships
+				RETURN {
+					partnership: p,
+					claim: c,
+					edge: e
+				}
+			`);
+			const indexResults = await indexCursor.all();
+
+			req.log.info({ indexResults }, "Claim indexed");
+
+			return res.json({
+				success: true,
+				data: {
+					to,
+					amount: rewardsToPay
+				}
+			});
 		} catch (e) {
 			handleException(e);
 			req.log.error(
@@ -177,6 +305,8 @@ handler.use(withAuth).post(async (req: AuthApiRequest, res: ApiResponse) => {
 			});
 		}
 	}
+
+	req.log.warn("Unsupported partnership");
 
 	return res.json({
 		success: false,
