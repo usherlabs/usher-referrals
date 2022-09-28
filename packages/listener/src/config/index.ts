7f53c52a8c1d24fdc76731cfd49168ed19918477
Victor Shevtsov <victorshevtsov@gmail.com>
2022-09-28 12:33:44 +0000

    add `pino` logger

--- packages/listener/src/config/index.ts
+++ packages/listener/src/config/index.ts
@@ -1,4 +1,11 @@
 import 'dotenv/config';
+const pkg = require("../../package.json");
+
+export const isProd = process.env.NODE_ENV === "production";
+export const logLevel = process.env.LOG_LEVEL || "info";
+export const appPackageName = pkg.name;
+export const appVersion = pkg.version;
+export const appName = `${appPackageName}@${appVersion}`;
 
 export const providerUrl = process.env.PROVIDER_URL;
 export const arangoUrl = process.env.ARANGO_URL || "http://127.0.0.1:8529";
