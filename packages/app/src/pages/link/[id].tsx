50f1e39aed97e092c74598d70b5eeca6eb61206a
Victor Shevtsov <victorshevtsov@gmail.com>
2023-01-22 22:48:29 +0000

    Update Link processing page

--- packages/app/src/pages/link/[id].tsx
+++ packages/app/src/pages/link/[id].tsx
@@ -1,26 +1,72 @@
-import { TileDocument } from "@ceramicnetwork/stream-tile";
-import { Heading, Pane } from "evergreen-ui";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
 import { ceramic } from "@/utils/ceramic-client";
+import { TileDocument } from "@ceramicnetwork/stream-tile";
+import { Connections } from "@usher.so/shared";
+import { Pane } from "evergreen-ui";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+
+import LogoImage from "@/assets/logo/Logo.png";
+import WalletInvite from "@/components/WalletInvite";
+
+type Link = {
+	id: string;
+	destination_url: string;
+	connections: Connections[];
+};
 
 const LinkPage: React.FC = () => {
 	const router = useRouter();
 	const id = router.query.id as string;
-	const [link, setLink] = useState<unknown>();
+	const [link, setLink] = useState<Link>();
+	const [domain, setDomain] = useState<string>();
 
 	useEffect(() => {
 		(async () => {
-			const doc = await (await TileDocument.load(ceramic, id)).content;
+			const doc = (await TileDocument.load(ceramic, id)).content as Link;
 			setLink(doc);
+			if (doc) {
+				setDomain(new URL(doc.destination_url).hostname);
+			}
+
+			// TODO: Call the API to register Link Hit
 		})();
-	}, []);
+	}, [id]);
+
+	const onWalletConnect = useCallback(async () => {
+		// TODO: Call the API to collect connected wallet
+
+		if (link) {
+			window.location.replace(link?.destination_url);
+		}
+	}, [link]);
 
 	return (
-		<Pane>
-			<Heading>{id}</Heading>
-			<pre>{JSON.stringify(link, null, 2)}</pre>
+		<Pane
+			display="flex"
+			flexDirection="column"
+			marginY="0"
+			marginX="auto"
+			minHeight="100vh"
+			position="relative"
+		>
+			<WalletInvite
+				domain={domain as string}
+				connections={link?.connections}
+				onConnect={onWalletConnect}
+			/>
+			<Pane
+				zIndex={100}
+				position="fixed"
+				bottom={20}
+				left={0}
+				right={0}
+				display="flex"
+				alignItems="center"
+				justifyContent="center"
+			>
+				<Image src={LogoImage} width={120} objectFit="contain" />
+			</Pane>
 		</Pane>
 	);
 };
